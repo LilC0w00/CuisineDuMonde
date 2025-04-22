a9c8f6b4ccdc5913bfb348fd6e7adb8b48cda3ba
Salma <dreambellytb@gmail.com>
2025-04-22 19:27:32 +0200

    éviter faille XXS

--- backend/htdocs/admin.login.php
+++ backend/htdocs/admin.login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+echo htmlspecialchars($_POST['commentaire']);
+
 $pdo = new PDO("mysql:host=localhost;dbname=cuisine_du_monde", "root", "");
 
 // Sécurité à améliorer, mais on reste simple pour l’instant
