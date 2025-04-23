1269db7b816eb3f1900515fac46cf5ad19bce7d9
Salma <dreambellytb@gmail.com>
2025-04-23 19:17:03 +0200

    Css link pour create-admin

--- backend/htdocs/create-admin.php
+++ backend/htdocs/create-admin.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Créer un compte</title>
 
-  <link rel="stylesheet" href="logcrea.css">
+  <link rel="stylesheet" href="../../frontend/css/logcrea.css">
 </head>
 
 <body>
